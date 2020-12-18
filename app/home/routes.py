5dd27699d829f91c5ec101211314b1dfa21b6141
Sivadinesh M <sivadinesh@gmail.com>
2020-12-19 02:32:14 +0530

    changes in dashboard, mission and contact screens

--- app/home/routes.py
+++ app/home/routes.py
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from app.home import blueprint
 from flask import render_template, redirect, url_for, request
+import requests
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
@@ -15,6 +16,26 @@ def index():
 
     return render_template('index.html', segment='index')
 
+@blueprint.route('/mission')
+@login_required
+def missionApi():
+    # response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    response = requests.get('http://localhost:3000/dataPackage')
+    
+
+    
+    
+    if response.status_code != 200:
+        # This means something went wrong.
+        print('{} {}'.format(response.status_code))
+    
+    package_data_list = response.json()
+
+    package_data_list1 = response.json()
+    
+
+    return render_template('mission.html', package_data_list = package_data_list,  package_data_list1 = package_data_list1 )
+    
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
@@ -50,3 +71,9 @@ def get_segment( request ):
 
     except:
         return None  
+
+# @app.route("/users", methods=['GET'])
+# def get_user():
+#     return {
+#         "user": "John Doe",
+#     }
